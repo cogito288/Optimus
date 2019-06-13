817cc956d271d0b4bf9f120286511b771d1e8ee5
cogito <tkdtkdghkt@gmail.com>
2019-06-13 20:52:36 +0900

    modify image in measurement/image

--- measurement/image/build.sh
+++ measurement/image/build.sh
@@ -1,4 +1,8 @@
 # build and push
-ansible all -m shell -a "docker rmi -f xxx/k8s-mxnet-measurement:latest"
-docker build -t xxx/k8s-mxnet-measurement:latest -f k8s-mxnet-measurement.Dockerfile .
-docker push xxx/k8s-mxnet-measurement:latest
+#ansible all -m shell -a "docker rmi -f xxx/k8s-mxnet-measurement:latest"
+#docker build -t xxx/k8s-mxnet-measurement:latest -f k8s-mxnet-measurement.Dockerfile .
+#docker push xxx/k8s-mxnet-measurement:latest
+
+docker rmi -f cogito288/k8s-mxnet-measurement:latest
+docker build -t cogito288/k8s-mxnet-measurement:latest -f k8s-mxnet-measurement.Dockerfile .
+docker push cogito288/k8s-mxnet-measurement:latest
